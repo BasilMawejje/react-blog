15d748d8ef4c88af4c8e993b39276a5535447dcb
BasilMawejje <basil.mawejje@gmail.com>
2019-12-19 19:24:53 +0300

    Rename action types to actual action names Add edit action spec All tests green

--- src/actions/fetchPosts.test.js
+++ src/actions/fetchPosts.test.js
@@ -2,8 +2,8 @@ import thunk from "redux-thunk";
 import MockAdapter from "axios-mock-adapter";
 import configureStore from "redux-mock-store";
 
-import { fetchPosts, fetchPost } from "./index";
-import { FETCH_POSTS, FETCH_POST } from "./types";
+import { fetchPosts, fetchPost, createPost } from "./index";
+import { FETCH_POSTS, FETCH_POST, CREATE_POST } from "./types";
 import { jsonPlaceholder } from "../apis/jsonPlaceholder";
 
 describe("Fetch posts", () => {
@@ -13,6 +13,11 @@ describe("Fetch posts", () => {
     httpMock = new MockAdapter(jsonPlaceholder);
     const mockStore = configureStore([thunk]);
     store = mockStore({});
+  });
+
+  afterEach(() => {
+    store.clearActions();
+    httpMock.reset();
   });
 
   it("Should return all posts", () => {
@@ -43,6 +48,23 @@ describe("Fetch posts", () => {
     const expectedAction = [{ type: FETCH_POST, payload: response }];
     httpMock.onGet(`/posts/${response.posts.id}`).reply(200, response);
     store.dispatch(fetchPost(response.posts.id)).then(() => {
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  it('Should create a post', () => {
+    const response = {
+      posts: {
+        id: 1,
+        title: "Created title",
+        body: "Created body"
+      }
+    };
+
+    const expectedAction = [{ type: CREATE_POST, payload: response.posts }];
+    httpMock.onPost('/posts', {id: 1, title: "Created title", body: 'Created body'}).reply(201, {id: 1, title: "Created title", body: 'Created body'});
+
+    store.dispatch(createPost({id: 1, title: "Created title", body: 'Created body'})).then(() => {
       expect(store.getActions()).toEqual(expectedAction);
     });
   });
