8fac1bbbfedda3ceb96d9e65f6fb99e20efb09c2
BasilMawejje <basil.mawejje@gmail.com>
2020-01-09 13:06:18 +0300

    Add postedit tests

---
+++ src/components/PostEdit.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux'
+import { shallowToJson } from 'enzyme-to-json';
+
+import PostEdit from './PostEdit'
+
+configure({ adapter: new Adapter() });
+
+const mockStore = configureStore();
+const store = mockStore({posts: 
+    [{ id: 1, title: 'Some post', body: 'lorem ipsum ...' }]
+});
+
+describe('PostEdit', () => {
+    let wrapper; 
+
+    beforeEach(() => {
+        wrapper = shallow(
+            <Provider store={store}>
+                <PostEdit />
+            </Provider>
+        );
+    });
+
+    it('renders properly', () => { 
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+})
